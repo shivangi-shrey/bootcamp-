1681642d703696f9829090cfd9de7f833b4ed90a
Shivangi <shivangikris123@gmail.com>
2022-06-27 15:21:05 +0530

    ok

--- foldarray.js
+++ foldarray.js
@@ -37,3 +37,4 @@ console.log(arr.length);
 for(let i=0;i<arr.length;i++){
     console.log(arr[i])
 }
+
---
+++ pattern.js
@@ -0,0 +1,6 @@
+let n=5
+for(let i=0;i<=n;i++){
+    for(j=1;j<=i+1;i++){
+        console.log(j)
+    }
+}
