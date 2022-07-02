7eb31ad63a8094dd9adcb314880daf00d17f4511
Shivangi <shivangikris123@gmail.com>
2022-07-02 14:15:43 +0530

    ok

--- dice.js
+++ dice.js
@@ -1,4 +1,4 @@
-let fs = require("fs");
+*let fs = require("fs");
 let data = fs.readFileSync(0, 'utf-8');
 let idx = 0;
 data = data.split('\n');
@@ -11,18 +11,11 @@ function readLine() {
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 let n = parseInt(readLine());
-let arr =[]
+let matrix=[];
 for(let i=0;i<n;i++){
-  
- let k= readLine().split(" ");
- arr.push(k)
- console.log(k)
+    matrix.push(parseInt(readLine()))
+    //console.log(matrix)
 }
+for(let i=0;i<matrix.length;i++){
 
-let max= -999999999
-for(let i=0;i<arr.length;i++){
-    if(max<arr[i]){
-    arr[i]=max
-    }
 }
-  // console.log([i])
