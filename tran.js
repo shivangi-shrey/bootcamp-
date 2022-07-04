cbfab7075943ee629aeff5ddd5ef51cde4c19725
Shivangi <shivangikris123@gmail.com>
2022-07-04 21:22:25 +0530

    ok

---
+++ tran.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1].trim();
+}
+
+// -------- Do NOT edit anything above this line ----------
+function findseat(person){
+    if( person>60&&person==M){
+        
+    }
+
+
+}
